b85789739845bd2ca78f0dba08161b1dcefcb2cf
Robert Poręba <webberr14@gmail.com>
2021-08-08 16:05:06 +0200

    08.08 #change layout

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import Layout from '../../components/Layout';
+import React from "react";
+import Layout from "../../components/Layout";
 
-import * as styles from '../../styles/project.module.css';
+import * as styles from "../../styles/project.module.css";
 
 export default function index() {
-	return (
-		<Layout>
-			<div className={styles.portfolio}>
-				<h2>Projects</h2>
-				<h3>title</h3>
-			</div>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <div className={styles.portfolio}>
+        <h2>Projects</h2>
+        <h3>title</h3>
+      </div>
+    </Layout>
+  );
 }
