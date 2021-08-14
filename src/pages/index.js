7b27c39230961ad713696d0899b6b2438c8eabac
Robert Poręba <webberr14@gmail.com>
2021-08-14 17:20:14 +0200

    14.08 #add useStaticQuery hook

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
-import { graphql, Link } from "gatsby";
+import { Link } from "gatsby";
 import React from "react";
 import Layout from "../components/Layout";
 
 import * as styles from "../styles/home.module.css";
 
-export default function Home({ data }) {
-  const { description, title } = data.site.siteMetadata;
-
+export default function Home() {
   return (
     <Layout>
       <section className={styles.header}>
@@ -19,21 +17,7 @@ export default function Home({ data }) {
           </Link>
         </div>
         <img src="/banner.png" alt="site banner" style={{ maxWidth: "100%" }} />
-        <p>
-          {title} - {description}
-        </p>
       </section>
     </Layout>
   );
 }
-
-export const query = graphql`
-  query SiteInfo {
-    site {
-      siteMetadata {
-        description
-        title
-      }
-    }
-  }
-`;
